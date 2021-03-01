806d3c29f1d644fa5584d5b968d87951faab086d
John Zhang <1724118498@qq.com>
2021-03-01 11:58:51 +0800

    first prototype done

---
+++ src/index.js
@@ -0,0 +1,70 @@
+const http = require('http');
+
+const url = require('url');
+const query = require('querystring');
+
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./responses.js');
+
+// 3 - locally this will be 3000, on Heroku it will be assigned
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': htmlHandler.getHomeClientResponse,
+  '/link-submit': htmlHandler.getLinkSubmitClientPageResponse,
+  '/links-client': htmlHandler.getLinksClientResponse,
+  '/home-client':htmlHandler.getHomeClientResponse,
+  '/get-links': jsonHandler.getLinks,
+  notFound: htmlHandler.get404Response,
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/add-link') {
+    const body = [];
+
+    // https://nodejs.org/api/http.html
+    request.on('error', (err) => {
+      console.dir(error);
+      response.statusCode = 400;
+      response.end();
+    });
+
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString(); // name=tony&age=35
+      const bodyParams = query.parse(bodyString); // turn into an object with .name & .age
+      jsonHandler.addLink(request, response, bodyParams);
+    });
+  }
+};
+
+const onRequest = (request, response) => {
+  let acceptedTypes = request.headers.accept && request.headers.accept.split(',');
+  acceptedTypes = acceptedTypes || [];
+
+  const parsedUrl = url.parse(request.url);
+  const {
+    pathname,
+  } = parsedUrl;
+
+  if (request.method === 'POST') {
+    // handle POST
+    handlePost(request, response, parsedUrl);
+    return; // bail out of function
+  }
+
+  const params = query.parse(parsedUrl.query);
+  const httpMethod = request.method;
+
+  if (urlStruct[pathname]) {
+    urlStruct[pathname](request, response, params, acceptedTypes, httpMethod);
+  } else {
+    urlStruct.notFound(request, response);
+  }
+};
+
+// 8 - create the server, hook up the request handling function, and start listening on `port`
+http.createServer(onRequest).listen(port);
---
+++ src/responses.js
@@ -0,0 +1,65 @@
+const linknote = {
+  'links': [
+    {
+      link: 'https://people.rit.edu/jz2728',
+      name: 'My first link',
+      note: 'This is the default link',
+    }
+  ],
+};
+
+const sendJSONResponse = (request, response, responseCode, object) => {
+  response.writeHead(responseCode, {
+    'Content-Type': 'application/json'
+  });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+// "Meta" refers to *meta data*, in this case the HTTP headers
+const sendJSONResponseMeta = (request, response, responseCode) => {
+  response.writeHead(responseCode, {
+    'Content-Type': 'application/json'
+  });
+  response.end();
+};
+
+const getLinks = (request, response, params) => {
+  sendJSONResponse(request, response, 200, linknote);
+};
+
+const addLink = (request, response, body) => {
+  // here we are assuming an error, pessimistic aren't we?
+  let responseCode = 400; // 400=bad request
+  const responseJSON = {
+    id: 'missingParams',
+    message: 'name and age are both required',
+  };
+
+  // missing name or age?
+  if (!body.name || !body.link) {
+    return sendJSONResponse(request, response, responseCode, responseJSON);
+  }
+
+  // we DID get a name and age
+  if (linknote.links[1]) { // if the user exists
+    responseCode = 204;
+    linknote.links[1].link = body.link; // update
+    return sendJSONResponseMeta(request, response, responseCode);
+  }
+
+  // if the user does not exist
+  linknote.links[1] = {}; // make a new user
+  // initialize values
+  linknote.links[1].name = body.name;
+  linknote.links[1].link = body.link;
+  linknote.links[1].note = body.note;
+
+  responseCode = 201; // send "created" status code
+  responseJSON.id = linknote.links[1].name;
+  responseJSON.message = 'Created Successfully';
+  return sendJSONResponse(request, response, responseCode, responseJSON);
+};
+
+module.exports.getLinks = getLinks;
+module.exports.addLink = addLink;
